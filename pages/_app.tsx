9258c75398100f641959413e17e0fcfa66961dc4
fumiya0 <46877325+fumiya0@users.noreply.github.com>
2020-09-01 12:54:05 +0900

    bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../tailwind.css";
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
 
 import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
